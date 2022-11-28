1886c6cfdcf0ee62f0d15fb7324ccb7c9c1dc336
deadex-ng <thisisfumba@gmail.com>
2022-11-28 18:30:42 +0200

    french translator

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({userInput}),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
   return (
     <div className="root">
       <Head>
@@ -11,12 +39,37 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>English To French Translator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>The AI Translator - your perfect solution for communicating in French!</h2>
           </div>
         </div>
+        <textarea
+          className="prompt-box"
+          placeholder="I would like to travelaround the world ans see all the wonderful places"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={callGenerateEndpoint}>
+            <div className="generate">
+              <p>Translate</p>
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+        <div className="output-content">
+          <p>{apiOutput}</p>
+        </div>
+      </div>
+      )}
       </div>
       <div className="badge-container grow">
         <a
